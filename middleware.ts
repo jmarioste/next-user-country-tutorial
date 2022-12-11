20d9424e00ba70c014d7119312ec06317559f819
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-12-11 15:05:37 +0800

    Refactoring

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,10 @@ import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
 const RESTRICTED_COUNTRIES = ["PH", "US"];
 export async function middleware(request: NextRequest, _next: NextFetchEvent) {
   const res = NextResponse.next();
-  const loc = request.cookies.get("country");
+  const country = request.cookies.get("country")?.value ?? "";
   //get the ip address depending on your hosting provider.
   const ip = request.ip;
-  let country = "";
-  if (!loc) {
+  if (!country) {
     try {
       const response = await fetch(`https://ipapi.co/${ip}/country/`);
       const country = await response.text();
