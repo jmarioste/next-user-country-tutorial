6f45e458570df51ed36610871b5004fcc1b57f3c
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-12-11 14:47:38 +0800

    test

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,23 @@
 import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
-
+const RESTRICTED_COUNTRIES = ["PH", "US"];
 export async function middleware(request: NextRequest, _next: NextFetchEvent) {
   const res = NextResponse.next();
-  let ip = request.ip ?? request.headers.get('x-real-ip');
-  
-  const forwardedFor = request.headers.get('x-forwarded-for')
-  if(!ip && forwardedFor){
-    ip = forwardedFor.split(',').at(0) ?? 'Unknown'
+  const loc = request.cookies.get("country");
+  //get the ip address depending on your hosting provider.
+  const ip = request.ip;
+  let country = "";
+  if (!loc) {
+    try {
+      const response = await fetch(`https://ipapi.co/${ip}/country/`);
+      const country = await response.text();
+      if (country) {
+        res.cookies.set("country", country);
+      }
+    } catch (error) {}
   }
-  
-  if(ip){
-    res.cookies.set("user-ip", ip, {
-      httpOnly: false,
-    });
+
+  if (RESTRICTED_COUNTRIES.includes(country)) {
+    return NextResponse.rewrite(new URL("/restricted", request.url));
   }
-  
   return res;
 }
